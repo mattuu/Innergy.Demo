95d681ef510c16a87cce489bdaa625ed4204d3b9
mattuu <matt@codemagix.co.uk>
2020-05-25 09:49:45 +0100

    Initial commit

---
+++ src/Innergy.Demo.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Innergy.Demo.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = Path.Combine(@"C:\tmp", "input.txt");
+
+            using(var file = File.OpenText(filePath)){
+                System.Console.WriteLine(file.ReadToEnd());
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
